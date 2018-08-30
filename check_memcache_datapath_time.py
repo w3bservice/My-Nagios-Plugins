c275439d7ce45f7dc605a50c4062ea4e82b78f6d
Jack Su <chnjackso@hotmail.com>
2018-08-31 09:35:22 +1000

    Added Nagios plugin check_memcache_datapath_time.py v1.0.0

---
+++ check_memcache_datapath_time.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import memcache,sys,datetime
+
+memcache_server="numown.docker.otw.net.au:8008"
+
+conn = memcache.Client([memcache_server])
+status = conn.get_stats()
+if status == []:
+	print ("%s connect failed!" % memcached_server)
+	sys.exit(1)
+else:
+	datapath_timestamp = float(status[0][1]['datapath'])
+	datapath_time = datetime.datetime.fromtimestamp(datapath_timestamp)
+	chk_time = datetime.datetime.now() - datetime.timedelta(days=1)
+	exist_time = (datetime.datetime.now() - datapath_time)
+	run_time = (str(exist_time)).split(".")[0]
+
+	if chk_time < datapath_time:
+		print "OK: datapath time is %s" % run_time
+		sys.exit(0)
+	else:
+		print "Error: datapath time is %s" % run_time
+		sys.exit(2)
